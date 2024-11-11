3e8c11e13c6d21294a7b9fae3d2978d499d4674b
CarloDettori <dettoric07@gmail.com>
2024-11-11 14:43:17 +0100

    creating cicle to define extrated numbers

--- script/script.js
+++ script/script.js
@@ -6,3 +6,12 @@ let userGuess = [];
 
 // array vuoto per i numeri indovinati
 let userRightAns = [];
+
+// ciclo per definire i numeri estratti
+while (extration < 5) {
+    extration.push(generaNumeroRandom(1, 100));
+}
+console.log(extration)
+
+
+
--- script/utility.js
+++ script/utility.js
@@ -33,34 +33,3 @@ function isEven(num) {
     return num % 2 === 0;
 }
 
-function isPalindrom() {
-    for (let i = word.length - 1; i >= 0; i--) {
-        palindrom = palindrom + word[i];
-    }
-    console.log(word);
-    console.log(palindrom);
-
-    if (word == palindrom) {
-        console.log("le parole sono palindrome")
-    } else {
-        console.log("le non parole sono palindrome")
-    }
-}
-
-function winner() {
-    let parDisCont = (numUser + numPc) % 2;
-    let a = "";
-    if (parDisCont == 0) {
-        a = "pari";
-    } else {
-        a = "dispari";
-    }
-    console.log(`E' uscito ${a}`)
-    if (a == parDis) {
-        console.log("Congratulazioni!, hai vinto!")
-    } else {
-        console.log("Ritenta, sarai più fortunato")
-    }
-
-
-}
